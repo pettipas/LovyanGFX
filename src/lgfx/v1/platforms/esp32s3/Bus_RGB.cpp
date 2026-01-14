2b66622c965e1d0e5e73fd16e7fb6529e9bd95f0
Paul Pettipas <paul.pettipas@gmail.com>
2026-01-13 23:52:43 -0500

    Update Bus_RGB.cpp
    patching this

--- src/lgfx/v1/platforms/esp32s3/Bus_RGB.cpp
+++ src/lgfx/v1/platforms/esp32s3/Bus_RGB.cpp
@@ -95,7 +95,11 @@ namespace lgfx
 
   static void _gpio_pin_sig(uint32_t pin, uint32_t sig)
   {
+  #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 0, 0)
+    gpio_hal_func_sel((gpio_num_t)pin, PIN_FUNC_GPIO);
+  #else
     gpio_hal_iomux_func_sel(GPIO_PIN_MUX_REG[pin], PIN_FUNC_GPIO);
+  #endif
     gpio_set_direction((gpio_num_t)pin, GPIO_MODE_OUTPUT);
     esp_rom_gpio_connect_out_signal(pin, sig, false, false);
   }
